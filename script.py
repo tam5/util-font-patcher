552fafb37383aa89b356b18c498ab4739d395764
tam5 <arimiller92@gmail.com>
2020-01-03 16:11:40 -0500

    more

--- script.py
+++ script.py
@@ -23,21 +23,46 @@ for currentArgument, currentValue in arguments:
 print('--- Opening up a file')
 print(colored('hello', 'green'))
 
-# font = fontforge.open("/home/Arial.ttf")
+font = fontforge.open("/home/Arial.ttf")
+factor = 1.5
 
-# print(font.familyname)
 
-# # font.fontname = "NewFont"
-# # font.familyname =
-# # font.save("/home/NewFont.ttf")
+def adjust(font, attribute, factor):
+    original = getattr(font, attribute)
+    new = int(getattr(font, attribute) * factor)
 
-# # Fontname
-# font.fontname = "NewFont2"
-# # Family Name
-# font.familyname = "NewFont"
-# # Name For Humans
-# font.fullname = "NewFont1"
-# # Copyright
-# font.copyright = "(c) {} Acme Corp. All Rights Reserved.".format(datetime.now().year)
+    print("Adjusting {}: {} -> {}".format(attribute, original, new))
+    setattr(font, attribute, new)
+
+
+props = [
+    'os2_winascent',
+    'os2_windescent'
+    'os2_typoascent',
+    'os2_typodescent',
+    'hhea_ascent',
+    'hhea_descent'
+]
+
+for prop in props:
+    adjust(font, prop, factor)
+
+# print("Ascent: {}".format(font.ascent))
+# print("Descent: {}".format(font.descent))
+
+# print("Win Ascent: {} -> {}".format(font.os2_winascent, 1.1 * font.os2_winascent))
+# print("Win Descent: {} -> {}".format(font.os2_windescent, 1.1 * font.os2_windescent))
+
+# print(getattr(font, 'os2_winascent'))
+# adjust(font, 'os2_windescent', factor)
+
+# Fontname
+font.fontname = "NewFont2"
+# Family Name
+font.familyname = "NewFont"
+# Name For Humans
+font.fullname = "NewFont1"
+# Copyright
+font.copyright = "(c) {} Acme Corp. All Rights Reserved.".format(datetime.now().year)
 
 # font.save("/home/NewFont.ttf")
